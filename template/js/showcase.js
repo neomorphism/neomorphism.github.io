4c1061111dbd135f94bd466d0e2a13dbd566db6a
parkchangwoo1 <wkwmdsk808@ajou.ac.kr>
2021-06-07 21:23:21 +0900

    fix: showcase.js

--- template/js/showcase.js
+++ template/js/showcase.js
@@ -1,27 +1,24 @@
-const hideList = document.querySelectorAll('.hide');
-        const showHide = function(){
-            if(hideList){
-             hideList.forEach((element) =>{
-                 console.log(window.innerHeight );
-                 console.log(element.getBoundingClientRect().top);
-                if (window.innerHeight > element.getBoundingClientRect().top) {
-                        element.classList.add('show');
-                }
-             })
-         }
-         
-         }
-window.addEventListener('scroll', showHide);
+const hideList = document.querySelectorAll(".hide");
+const showHide = function () {
+  if (hideList) {
+    hideList.forEach((element) => {
+      if (window.innerHeight > element.getBoundingClientRect().top) {
+        element.classList.add("show");
+      }
+    });
+  }
+};
+window.addEventListener("scroll", showHide);
 
 const nav = document.querySelectorAll(".nav-link--animation");
-const section = document.querySelectorAll(".section"); 
+const section = document.querySelectorAll(".section");
 
-const scroll = function(nav,section){
-    nav.addEventListener('click', function(){
-                section.scrollIntoView(true);
-                })
-    }
-    var i;
-    for(i=1;i<nav.length;i++){
-        scroll(nav[i],section[i-1]);
+const scroll = function (nav, section) {
+  nav.addEventListener("click", function () {
+    section.scrollIntoView(true);
+  });
+};
+var i;
+for (i = 1; i < nav.length; i++) {
+  scroll(nav[i], section[i - 1]);
 }
